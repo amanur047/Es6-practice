15f6ce01d743f634193a7bdd21e3c27528733eea
amanur047 <amanur047@gmail.com>
2021-02-04 21:01:04 +0600

    fixed

---
+++ desconstructor.js
@@ -0,0 +1,4 @@
+const person = { name:"Jack William", age: 17 , job:"faceBooker", GF:"Emma Watson", address: "Kochu-khet", phone: "01732169965" };
+const gfName = person.GF;
+const phone = person.phone;
+console.log(gfName,phone);
